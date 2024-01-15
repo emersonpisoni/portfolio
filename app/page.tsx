88c6ccd77cef5cd3783c5c5a0a800eab145c228d
emersonpisoni <emersonpisoni14@gmail.com>
2024-01-15 13:42:26 -0300

    add computer animation zoom

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Button from '@/components/button.component'
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className='w-full bg-teal-900 h-16 flex justify-evenly items-center'>
-        <Button>Sobre</Button>
-        <Button>Skills</Button>
-        <Button>Projetos</Button>
+    <main className="bg-space flex min-h-screen flex-col relative z-0 items-center justify-center bg-slate-950">
+      <div className=' relative flex justify-center'>
+        <div className="flex flex-col items-center relative justify-between w-96 h-60 bg-slate-200 border-8 border-black rounded shadow-lg shadow-cyan-500/50 ">
+          <div className='w-full bg-teal-900 h-16 flex justify-evenly items-center blur '>
+            <Button>Sobre</Button>
+            <Button>Skills</Button>
+            <Button>Projetos</Button>
+          </div>
+          <div className='w-2 h-2 bg-red-600 -bottom-2 right-5 absolute rounded-full '></div>
+        </div>
+        <div className='w-40 h-40 bg-slate-100 -bottom-8 absolute clip-path-pc-base -z-10'></div>
       </div>
     </main>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,19 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'space': "url('/images/space.jpg')"
+      },
+      animation: {
+        zoom: 'zoom 31s ease-in-out infinite'
+      },
+      keyframes: {
+        zoom: {
+          '0%, 100%': { transform: 'scale(3)' },
+          '50%': { transform: 'scale(1)' }
+        }
       },
     },
+
   },
   plugins: [],
 }
