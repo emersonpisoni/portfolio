27fc3c0c2c3dfb10ae3ef81c5d870aabb9a1a587
emersonpisoni <emersonpisoni14@gmail.com>
2024-01-17 23:47:45 -0300

    remove white spaces

--- components/computer.component.tsx
+++ components/computer.component.tsx
@@ -2,9 +2,9 @@ import Button from "./button.component";
 
 export default function Computer() {
   return (
-    < div className='animate-zoom w-screen h-screen scale-90 relative flex justify-center items-center' >
-      < div className="overflow-auto flex flex-col  items-center relative w-full h-full bg-white border-[20px] border-black rounded shadow-lg shadow-cyan-500/50 " >
-        <div className='w-full bg-gradient-to-b from-violet-950 to-violet-900 min-h-screen h-svh flex justify-evenly items-center  -m-1'>
+    <div className='animate-zoom w-screen h-screen scale-90 relative flex justify-center items-center'>
+      <div className="overflow-auto flex flex-col  items-center relative w-full h-full bg-white border-[20px] border-black rounded shadow-lg shadow-cyan-500/50">
+        <div className='w-full bg-gradient-to-b from-violet-950 to-violet-900 min-h-screen h-svh flex justify-evenly items-center -m-1'>
           <Button>Sobre</Button>
           <Button>Skills</Button>
           <Button>Projetos</Button>
