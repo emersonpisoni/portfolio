92235575d468b06b1d11bd8131e03f4d93a07c5f
emersonpisoni <emersonpisoni14@gmail.com>
2024-01-22 23:10:31 -0300

    change button place

--- components/computer.component.tsx
+++ components/computer.component.tsx
@@ -17,8 +17,8 @@ export default function Computer() {
       </div >
       <div className='w-2 h-4 bg-green-500 -bottom-0 right-10 absolute rounded-full'></div>
       <div className='w-2 h-3 bg-white -bottom-0 right-16 absolute rounded-full'></div>
-      <div className='w-2 h-3 bg-white -bottom-0 right-24 absolute rounded-full'></div>
       <div className='w-2 h-3 bg-white -bottom-0 right-20 absolute rounded-full'></div>
+      <div className='w-2 h-3 bg-white -bottom-0 right-24 absolute rounded-full'></div>
       <div className='w-1/4 h-40 bg-slate-100 -bottom-8 absolute clip-path-pc-base -z-10'></div>
     </div >
   )
