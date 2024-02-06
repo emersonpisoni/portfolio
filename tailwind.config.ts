14fdd821a945037f3a7ce20ba605c049a68ff28c
Emerson Pisoni <40845046+emersonpisoni@users.noreply.github.com>
2024-02-05 23:33:02 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
       keyframes: {
         zoom: {
           '0%': { transform: 'scale(.5)' },
-          '100%': { transform: 'scale(.9)' }
+          '100%': { transform: 'scale(.8)' }
         },
         blur: {
           '0%': { filter: 'blur(8px)' },
