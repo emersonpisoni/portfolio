547f2352dccab68788850bf5927e3cfd93673a2c
Emerson Pisoni <40845046+emersonpisoni@users.noreply.github.com>
2024-02-15 23:02:51 -0300

    Update button.component.tsx

--- components/button.component.tsx
+++ components/button.component.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export default function Button({ children }: PropsWithChildren) {
-  return <button className="bg-black bg-opacity-50 p-2 rounded-md">{children}</button>
+  return <button className="font-bold bg-black bg-opacity-50 p-2 rounded-md">{children}</button>
 }
