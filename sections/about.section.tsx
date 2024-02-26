04f942ddb29729a08db1a99eb2afc27568f7efb6
emersonpisoni <emersonpisoni14@gmail.com>
2024-02-26 18:20:39 -0300

    feat: change about section and scroll size

--- sections/about.section.tsx
+++ sections/about.section.tsx
@@ -10,7 +10,7 @@ import Node from '../public/images/node.png'
 export default function About() {
   const t = useTranslations('index');
   return (
-    <section id='section-about' className='bg-gradient-to-b from-violet-700 to-violet-800 w-full py-40 flex justify-center items-center'>
+    <section id='section-about' className='bg-gradient-to-b from-violet-700 to-violet-800 w-full py-40 h-[calc(100vh-48px)] flex justify-center items-center'>
       <Image src='https://avatars.githubusercontent.com/u/40845046?s=400&u=53469f66cc29d7eb4921603120b92639f09415f0&v=4' className='rounded-full pb-4' alt='user-image' width={200} height={200} />
       <div className='flex flex-col pl-24 w-[700px]'>
         <h1 className='text-6xl pb-8'>🙂 {t('title')}</h1>
