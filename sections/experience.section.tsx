6cacae77b77610c959c1b3efca4e3f89b6fcdf3b
emersonpisoni <emersonpisoni14@gmail.com>
2024-03-08 02:58:41 -0300

    feat: add cv download option and expand panel

--- sections/experience.section.tsx
+++ sections/experience.section.tsx
@@ -1,34 +1,78 @@
 import ExperienceDivider from '@/components/experience-divider.component'
+import DownloadIcon from '@/components/icons/download.icon';
 import { useTranslations } from 'next-intl';
-import React from 'react'
+import React, { useEffect, useReducer, useRef, useState } from 'react'
 
 export default function Experience() {
   const t = useTranslations('index.experience');
+  const ref = useRef<HTMLDivElement>(null)
+  const [collapsed, setCollapsed] = useState<boolean>(true)
+  const [currentHeight, setCurrentHeight] = useState<string | undefined>('400px')
+
+  const showMoreLessText = collapsed ? t('showMoreLessButton.more') : t('showMoreLessButton.less')
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeWindow)
+
+    return () => window.removeEventListener('resize', resizeWindow)
+  }, [])
+
+  function resizeWindow() {
+    if (ref.current && !collapsed) {
+      const height = ref.current?.scrollHeight.toString() + 'px'
+
+      setCurrentHeight(height)
+    }
+  }
+
+  function scrollTo() {
+    const element = document.getElementById('section-experience')
+
+    element?.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  function showHideXP(collapsed: boolean) {
+    if (ref.current) {
+      setCurrentHeight(collapsed ? '400px' : `${ref.current.scrollHeight}px`)
+      setCollapsed(collapsed)
+      if (collapsed) scrollTo()
+    }
+  }
+
   return (
-    <section id='section-experience' className='bg-gradient-to-b from-violet-800 to-violet-900 w-full py-20 flex justify-center items-center'>
-      <div id='experience-list' className='relative w-1/2 flex flex-col'>
-        <ExperienceDivider date='Nov 2019 - Jan 2024' company='CWI Software' role='Frontend Developer'>
-          <ul className="list-disc">
-            {t.rich('senior', {
-              li: (chunks) => <li>{chunks}</li>,
-            })}
-          </ul>
-        </ExperienceDivider>
-        <ExperienceDivider date='Jan 2019 - Nov 2019' company='CWI Software' role='Frontend Developer Junior'>
-          <ul className="list-disc">
-            {t.rich('junior', {
-              li: (chunks) => <li>{chunks}</li>,
-            })}
-          </ul>
-        </ExperienceDivider>
-        <ExperienceDivider date='Set 2018 - Dez 2018' company='CWI Software' role='Projeto Crescer'>
-          <ul className="list-disc">
-            {t.rich('crescer', {
-              li: (chunks) => <li>{chunks}</li>,
-              div: (chunks: any) => <div className='py-4'>{chunks}</div>
-            })}
-          </ul>
-        </ExperienceDivider>
+    <section id='section-experience' className={`bg-violet-800 w-full py-28 flex flex-col justify-center items-center`}>
+      <div className='flex w-1/2 pb-10 items-center gap-4 flex-wrap'>
+        <a className='p-4 rounded text-black font-bold transition-colors bg-violet-100 hover:bg-violet-200 flex justify-center items-center gap-4' href='/documents/cv_emerson_ptbr.pdf' download>CV Português<DownloadIcon width={20} height={20} /> </a>
+        <a className='p-4 rounded text-black font-bold transition-colors bg-violet-100 hover:bg-violet-200 flex justify-center items-center gap-4' href='/documents/cv_emerson_en.pdf' download>Resume English<DownloadIcon width={20} height={20} /></a>
+      </div>
+      <div className='relative w-1/2 flex flex-col'>
+        <div ref={ref} id='experience-list' style={{ maxHeight: currentHeight }} className={`transition-[max-height] duration-500  ${collapsed && 'overflow-hidden'} `}>
+          <ExperienceDivider date='Nov 2019 - Jan 2024' company='CWI Software' role='Frontend Developer'>
+            <ul className="list-disc">
+              {t.rich('senior', {
+                li: (chunks) => <li>{chunks}</li>,
+                div: (chunks: any) => <div className='py-4 font-bold text-xl'>{chunks}</div>
+              })}
+            </ul>
+          </ExperienceDivider>
+          <ExperienceDivider date='Jan 2019 - Nov 2019' company='CWI Software' role='Frontend Developer Junior'>
+            <ul className="list-disc">
+              {t.rich('junior', {
+                li: (chunks) => <li>{chunks}</li>,
+              })}
+            </ul>
+          </ExperienceDivider>
+          <ExperienceDivider date='Set 2018 - Dez 2018' company='CWI Software' role='Projeto Crescer'>
+            <ul className="list-disc">
+              {t.rich('crescer', {
+                li: (chunks) => <li>{chunks}</li>,
+                div: (chunks: any) => <div className='py-4 font-bold'>{chunks}</div>
+              })}
+            </ul>
+          </ExperienceDivider>
+        </div>
+        {<div className={`bg-gradient-to-b from-transparent ${collapsed ? 'to-violet-800' : 'to-transparent'} content-[""] w-full h-full absolute`}></div>}
+        <button onClick={() => showHideXP(!collapsed)} className='absolute -bottom-6 right-0  p-2 rounded'>{showMoreLessText}</button>
       </div>
     </section>
   )
--- sections/projects.section.tsx
+++ sections/projects.section.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <section id='section-projects' className='bg-gradient-to-b from-violet-900 to-violet-950 w-full h-[calc(100vh-48px)] flex justify-center items-center'>
+    <section id='section-projects' className='bg-gradient-to-b from-violet-800 to-violet-900 w-full h-[calc(100vh-48px)] flex justify-center items-center'>
       Projects
     </section>
   )
