590702f8df5cf49d64f8e490b970e0455d609a70
emersonpisoni <emersonpisoni14@gmail.com>
2024-03-13 00:08:58 -0300

    feat: add carousel to show projects

---
+++ components/carousel.component.tsx
@@ -0,0 +1,149 @@
+import React, { useCallback, useEffect, useRef } from 'react'
+import {
+  EmblaCarouselType,
+  EmblaEventType,
+  EmblaOptionsType
+} from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react'
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons
+} from './carousel-arrow-buttons.component'
+import { DotButton, useDotButton } from './carousel-dot-button.component'
+import { CarouselContent } from '@/sections/projects.section'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { usePathname } from 'next/navigation'
+
+const TWEEN_FACTOR_BASE = 0.84
+
+const numberWithinRange = (number: number, min: number, max: number): number =>
+  Math.min(Math.max(number, min), max)
+
+type PropType = {
+  slides: CarouselContent[]
+  options?: EmblaOptionsType
+}
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props
+  const pathname = usePathname()
+  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+  const tweenFactor = useRef(0)
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi)
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi)
+
+  const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
+    tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length
+  }, [])
+
+  const tweenOpacity = useCallback(
+    (emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
+      const engine = emblaApi.internalEngine()
+      const scrollProgress = emblaApi.scrollProgress()
+      const slidesInView = emblaApi.slidesInView()
+      const isScrollEvent = eventName === 'scroll'
+
+      emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+        let diffToTarget = scrollSnap - scrollProgress
+        const slidesInSnap = engine.slideRegistry[snapIndex]
+
+        slidesInSnap.forEach((slideIndex) => {
+          if (isScrollEvent && !slidesInView.includes(slideIndex)) return
+
+          if (engine.options.loop) {
+            engine.slideLooper.loopPoints.forEach((loopItem) => {
+              const target = loopItem.target()
+
+              if (slideIndex === loopItem.index && target !== 0) {
+                const sign = Math.sign(target)
+
+                if (sign === -1) {
+                  diffToTarget = scrollSnap - (1 + scrollProgress)
+                }
+                if (sign === 1) {
+                  diffToTarget = scrollSnap + (1 - scrollProgress)
+                }
+              }
+            })
+          }
+
+          const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current)
+          const opacity = numberWithinRange(tweenValue, 0, 1).toString()
+          emblaApi.slideNodes()[slideIndex].style.opacity = opacity
+        })
+      })
+    },
+    []
+  )
+
+  useEffect(() => {
+    if (!emblaApi) return
+
+    setTweenFactor(emblaApi)
+    tweenOpacity(emblaApi)
+    emblaApi
+      .on('reInit', setTweenFactor)
+      .on('reInit', tweenOpacity)
+      .on('scroll', tweenOpacity)
+  }, [emblaApi, tweenOpacity])
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((content, index) => (
+            <div className="embla__slide" key={index}>
+              <Link href={`${pathname}/${content.routeTo}`}>
+                <div className=' bg-black/10 rounded-[1.8rem]'>
+                  <Image
+                    className="embla__slide__img"
+                    src={content.image}
+                    alt="Your alt text"
+                  />
+                  <p className='py-4 pl-4 text-xl'>
+                    {content.title}
+                  </p>
+                  <p className='py-4 pl-4'>
+                    {content.description}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={'embla__dot'.concat(
+                index === selectedIndex ? ' embla__dot--selected' : ''
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default EmblaCarousel
--- sections/projects.section.tsx
+++ sections/projects.section.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import EmblaCarousel from '@/components/carousel.component'
+import { EmblaOptionsType } from 'embla-carousel'
+import TCC from '../public/images/tcc.png'
+import Crescer from '../public/images/aula-crescer.png'
+import ModelPerson from '../public/images/model-person.png'
+import { StaticImageData } from 'next/image'
+import { useTranslations } from 'next-intl'
+
+export type CarouselContent = {
+  image: StaticImageData,
+  title: string,
+  routeTo: string,
+  description: string
+}
 
 export default function Projects() {
+  const t = useTranslations('index.projects');
+
+  const OPTIONS: EmblaOptionsType = { loop: true }
+  const SLIDES: CarouselContent[] = [
+    {
+      image: ModelPerson,
+      title: t('crescer-final.carousel.title'),
+      routeTo: "crescer-final",
+      description: t('crescer-final.carousel.description')
+    },
+    {
+      image: TCC,
+      title: t('tcc.carousel.title'),
+      routeTo: "tcc",
+      description: t('tcc.carousel.description')
+    },
+    {
+      image: Crescer,
+      title: t('crescer.carousel.title'),
+      routeTo: "crescer",
+      description: t('crescer.carousel.description')
+    }
+  ]
+
   return (
     <section id='section-projects' className='bg-gradient-to-b from-violet-800 to-violet-900 w-full h-[calc(100vh-48px)] flex justify-center items-center'>
-      Projects
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
     </section>
   )
 }
