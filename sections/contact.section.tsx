ef6c64f4c1c6af919d7fdabf1322fa198bcb6257
emersonpisoni <emersonpisoni14@gmail.com>
2024-03-14 20:46:10 -0300

    feat: change contact style

--- sections/contact.section.tsx
+++ sections/contact.section.tsx
@@ -5,9 +5,9 @@ import { useTranslations } from "use-intl";
 export default function Contact() {
   const t = useTranslations('index.contact')
 
-  return <section id="section-contact" className="w-full bg-gradient-to-b from-violet-800 to-violet-900 flex flex-col items-center justify-center p-28">
+  return <section id="section-contact" className="w-full bg-gradient-to-b from-violet-800 to-violet-900 flex flex-col items-center justify-center pb-32 px-6">
     <h1 className="font-bold text-4xl">{t('title')}</h1>
-    <div className="p-20 flex items-center gap-4">
+    <div className="pt-6 flex items-center gap-4">
       <Link href={"https://www.linkedin.com/in/emersonpisoni/"} target="_blank" >
         <Image src='/images/linkedin.svg' alt="linkedin icon" width={70} height={80} />
       </Link>
